b43f6150e869621b687f52bff9502fc4199c709d
julia3106 <julia.miranda.luiz@escola.pr.gov.br>
2025-08-17 11:45:41 -0300

    funções

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 
 botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 
-function mostraPalavrasChave() {
+function mostraPalavrasDoTexto() {
     const texto = document.querySelector('#entrada-de-texto').value;
     const campoResultado = document.querySelector('#resultado-palavrachave');
     const palavrasChave = processaTexto(texto);
@@ -10,7 +10,7 @@ function mostraPalavrasChave() {
     campoResultado.textContent = palavrasChave.join(", ");
 }
 
-function processaTexto(texto) {
+function dividirTextoEmPalavras(texto) {
     let palavras = texto.split(/\P{L}+/u);
     return palavras;
 }
